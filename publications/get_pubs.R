b4c25f57bf5e6567943ff76e50e31b8824788e5a
Arya Zandvakili <28754724+aryazand@users.noreply.github.com>
2024-09-28 18:35:37 -0500

    updated website

---
+++ publications/get_pubs.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(rorcid)
+library(rcrossref)
+
+myorcid <- rorcid::works("0000-0001-8031-8067")
+
+dois <- myorcid |> 
+  pluck("external-ids.external-id") |> 
+  map(filter, `external-id-type` == "doi") |> 
+  bind_rows() |> 
+  pluck("external-id-value")
+
+biblio <- map(dois, rcrossref::cr_cn, format = "text", style = "apa")
+
+saveRDS(biblio, "publications/biblio.rds")
